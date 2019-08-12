93e50a9fc907ecd9bee43681deab2f8c77acb172
DivyaMahes <53871278+DivyaMahes@users.noreply.github.com>
2019-08-12 09:21:06 +0530

    Update Puzzle.java

--- Puzzle/src/PuzzlePackage/Puzzle.java
+++ Puzzle/src/PuzzlePackage/Puzzle.java
@@ -2,7 +2,6 @@ package PuzzlePackage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -15,45 +14,28 @@ import javax.swing.JOptionPane;
 		{
 		frame.setExtendedState(frame.MAXIMIZED_BOTH);
 		
-		b1=new JButton("5");
-		b1.setBounds(100,150,50,50);
-		b2=new JButton("3");
-		b2.setBounds(150,150,50,50);
-		b3=new JButton("2");
-		b3.setBounds(200,150,50,50);
-		b4=new JButton("1");
-		b4.setBounds(100,200,50,50);
-		b5=new JButton("4");
-		b5.setBounds(150,200,50,50);
-		b6=new JButton("7");
-		b6.setBounds(200,200,50,50);
-		b7=new JButton("6");
-		b7.setBounds(100,250,50,50);
-		b8=new JButton("");
-		b8.setBounds(150,250,50,50);
-		b9=new JButton("8");
-		b9.setBounds(200,250,50,50);
+		b1=new JButton("5"); b1.setBounds(100,150,50,50);
+		b2=new JButton("3"); b2.setBounds(150,150,50,50);
+		b3=new JButton("2"); b3.setBounds(200,150,50,50);
+		b4=new JButton("1"); b4.setBounds(100,200,50,50);
+		b5=new JButton("4"); b5.setBounds(150,200,50,50);
+		b6=new JButton("7"); b6.setBounds(200,200,50,50);
+		b7=new JButton("6"); b7.setBounds(100,250,50,50);
+		b8=new JButton(""); b8.setBounds(150,250,50,50);
+		b9=new JButton("8"); b9.setBounds(200,250,50,50);
 		
-		reset=new JButton("Reset");
-		reset.setBounds(150,350,75,40);
+		reset=new JButton("Reset"); reset.setBounds(150,350,75,40);
 		
 		frame.add(b1);frame.add(b2);frame.add(b3);frame.add(b4);frame.add(b5);
 		frame.add(b6);frame.add(b7);frame.add(b8);frame.add(b9);frame.add(reset);
 		
-		b1.addActionListener(this);
-		b2.addActionListener(this);
-		b3.addActionListener(this);
-		b4.addActionListener(this);
-		b5.addActionListener(this);
-		b6.addActionListener(this);
-		b7.addActionListener(this);
-		b8.addActionListener(this);
-		b9.addActionListener(this);
+		b1.addActionListener(this); b2.addActionListener(this); b3.addActionListener(this);
+		b4.addActionListener(this); b5.addActionListener(this); b6.addActionListener(this);
+		b7.addActionListener(this); b8.addActionListener(this); b9.addActionListener(this);
 		reset.addActionListener(this);
 		
 		frame.setLayout(null);
 		frame.setVisible(true);
-		
 		}
 		  public void actionPerformed(ActionEvent event)
 		  {   
@@ -225,15 +207,8 @@ import javax.swing.JOptionPane;
 		  
 		  if(event.getSource()==reset)
 		  {
-			  b1.setText("5");
-			  b2.setText("3");
-			  b3.setText("2");
-			  b4.setText("1");
-			  b5.setText("4");
-			  b6.setText("7");
-			  b7.setText("6");
-			  b8.setText("");
-			  b9.setText("8");
+			  b1.setText("5"); b2.setText("3"); b3.setText("2"); b4.setText("1");
+			  b5.setText("4"); b6.setText("7"); b7.setText("6"); b8.setText(""); b9.setText("8");
 		  }
 		
 		if(b1.getText()=="1"&&b2.getText()=="2"&&b3.getText()=="3"
@@ -251,10 +226,6 @@ import javax.swing.JOptionPane;
 		
 	public static void main(String[] args) 
 	{  
-	   Puzzle p= new Puzzle(); 
-	   
+	   Puzzle p= new Puzzle();   
 	} 
 	}
-	 
-
-
